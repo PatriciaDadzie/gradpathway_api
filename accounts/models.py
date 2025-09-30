1975cdb219250691ac1376ab4b5b34ba7e76afde
Patricia Dadzie <pdadzie502@gmail.com>
2025-09-30 03:29:06 +0000

    Add custom User model with country_preference

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,8 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
     country_preference = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
