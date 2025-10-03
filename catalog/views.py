1ee4202dfe9188aac23af253e5805c30d5e5d25f
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-03 09:44:10 +0000

    Add API views for University and Programme

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import University, Programme
+from .serializers import UniversitySerializer, ProgrammeSerializer
+
+class UniversityListCreateView(generics.ListCreateAPIView):
+    queryset = University.objects.all()
+    serializer_class = UniversitySerializer
+
+
+class ProgrammeListCreateView(generics.ListCreateAPIView):
+    queryset = Programme.objects.all()
+    serializer_class = ProgrammeSerializer
+
+
+class ProgrammeDetailView(generics.RetrieveAPIView):
+    queryset = Programme.objects.all()
+    serializer_class = ProgrammeSerializer
