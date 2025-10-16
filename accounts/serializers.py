e607c7de6896ef6be6b8b73501de7b6265179b90
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-16 10:14:57 +0000

    Refactor RegisterSerializer to remove username and use email as primary identifier

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,14 +4,15 @@ from .models import User
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "email", "password", "country_preference")
+        fields = ("id", "email", "password", "first_name", "last_name", "country_preference")
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data["username"],
             email=validated_data["email"],
             password=validated_data["password"],
+            first_name=validated_data.get("first_name", ""),
+            last_name=validated_data.get("last_name", ""),
             country_preference=validated_data.get("country_preference", "")
         )
         return user
