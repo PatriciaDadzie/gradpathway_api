9d8e4bf431011038c1b8ae555194fdf0025058f0
Patricia Dadzie <pdadzie502@gmail.com>
2025-10-17 10:45:32 +0000

    Fix login endpoint to use email instead of username

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,18 +1,40 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import User
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "email", "password", "first_name", "last_name", "country_preference")
+        fields = ("id", "username", "email", "password", "country_preference")
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
+        username = validated_data.get("username") or validated_data["email"].split("@")[0]
         user = User.objects.create_user(
+            username=username,
             email=validated_data["email"],
             password=validated_data["password"],
-            first_name=validated_data.get("first_name", ""),
-            last_name=validated_data.get("last_name", ""),
             country_preference=validated_data.get("country_preference", "")
         )
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({"email": "User with this email does not exist."})
+
+        user = authenticate(username=user.username, password=password)
+        if not user:
+            raise serializers.ValidationError({"password": "Invalid password."})
+        data["user"] = user
+        return data
--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,42 @@
-from rest_framework import generics
-from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from .models import User
-from .serializers import RegisterSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from .serializers import RegisterSerializer, LoginSerializer
 
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
-    permission_classes = [AllowAny]
 
-    def create(self, request, *args, **kwargs):
-        response = super().create(request, *args, **kwargs)
-        user = User.objects.get(id=response.data["id"])
-        token, _ = Token.objects.get_or_create(user=user)
-        response.data["token"] = token.key
-        return response
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key,
+                "user": {
+                    "id": user.id,
+                    "email": user.email,
+                    "username": user.username,
+                    "country_preference": user.country_preference
+                }
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data["user"]
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                "token": token.key,
+                "user": {
+                    "id": user.id,
+                    "email": user.email,
+                    "username": user.username,
+                    "country_preference": user.country_preference
+                }
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
